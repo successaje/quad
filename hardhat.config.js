fe9bd5847cfb33043bfb4ed6a8465ed20f7b4342
Success Aje <73680210+successaje@users.noreply.github.com>
2024-11-07 10:10:07 +0100

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,47 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  networks: {
+      alfajores: {
+          // can be replaced with the RPC url of your choice.
+          url: "https://alfajores-forno.celo-testnet.org",
+          accounts: [
+              "<YOUR_PRIVATE_KEY>"
+          ],
+      },
+      celo: {
+          url: "https://forno.celo.org",
+          accounts: [
+              "<YOUR_PRIVATE_KEY>"
+          ],
+      }
+  },
+  etherscan: {
+      apiKey: {
+          alfajores: "<CELOSCAN_API_KEY>",
+          celo: "<CELOSCAN_API_KEY>"
+      },
+      customChains: [
+          {
+              network: "alfajores",
+              chainId: 44787,
+              urls: {
+                  apiURL: "https://api-alfajores.celoscan.io/api",
+                  browserURL: "https://alfajores.celoscan.io",
+              },
+          },
+          {
+              network: "celo",
+              chainId: 42220,
+              urls: {
+                  apiURL: "https://api.celoscan.io/api",
+                  browserURL: "https://celoscan.io/",
+              },
+          },
+      ]
+  },
 };
